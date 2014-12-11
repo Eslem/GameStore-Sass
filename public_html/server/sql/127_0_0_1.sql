77563b5c05f903c5b1ab69b2b5f387ea61685c3e
jesuscc1993 <jesuscc1993@gmail.com>
2014-12-11 10:56:33 +0100

    Añadida tabla de relación producto/categoria

--- public_html/server/sql/127_0_0_1.sql
+++ public_html/server/sql/127_0_0_1.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-12-2014 a las 12:13:20
--- Versión del servidor: 5.6.17
--- Versión de PHP: 5.5.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `tienda`
---
 CREATE DATABASE IF NOT EXISTS `tienda` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `tienda`;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `administrador`
---
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'administrador'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `administrador` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -42,18 +23,18 @@ CREATE TABLE IF NOT EXISTS `administrador` (
   UNIQUE KEY `alias` (`alias`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
---
--- Volcado de datos para la tabla `administrador`
---
+
+--------------------------------------------------------------------------------
+-- Inserción de tuplas en la tabla 'administrador'
+--------------------------------------------------------------------------------
 
 INSERT INTO `administrador` (`id`, `nombre`, `apellido`, `direccion`, `telefono`, `email`, `alias`, `password`) VALUES
 (1, 'admin', 'admin', 'admin', 'admin', 'admin', 'admin', 'admin');
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `categoria`
---
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'categoria'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `categoria` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -62,11 +43,10 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `linea_pedido`
---
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'linea_pedido'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `linea_pedido` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -77,11 +57,10 @@ CREATE TABLE IF NOT EXISTS `linea_pedido` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `pedido`
---
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'pedido'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `pedido` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -90,35 +69,33 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `producto`
---
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'producto'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `producto` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `descripcion` varchar(255) NOT NULL,
   `precio` float NOT NULL,
-  `categorias` varchar(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `nombre` (`nombre`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
---
--- Volcado de datos para la tabla `producto`
---
 
-INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`, `categorias`) VALUES
-(1, 'Farming Simulator 2014', 'Simuladorzaco', 24.99, 'Simulador');
+--------------------------------------------------------------------------------
+-- Inserción de tuplas en la tabla 'producto'
+--------------------------------------------------------------------------------
 
--- --------------------------------------------------------
+INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`) VALUES
+(1, 'Farming Simulator 2014', 'Simuladorzaco', 24.99);
 
---
--- Estructura de tabla para la tabla `usuario`
---
+
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'usuario'
+--------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -134,6 +111,15 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `alias` (`alias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--------------------------------------------------------------------------------
+-- Creación de la tabla de relación 'producto_categorias'
+--------------------------------------------------------------------------------
+
+CREATE TABLE `producto_categorias` (
+	`idProducto` INT(11) NULL DEFAULT NULL,
+	`idCategoria` INT(11) NULL DEFAULT NULL,
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB AUTO_INCREMENT=4;
