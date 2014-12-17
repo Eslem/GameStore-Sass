9690c1c16e42c7ff080f1dcc2bafa037122c0a12
jesuscc1993 <jesuscc1993@gmail.com>
2014-12-17 12:19:39 +0100

    Added error control when request to controller are missing parameters.

--- public_html/cliente/php/controller/genericController.php
+++ public_html/cliente/php/controller/genericController.php
@@ -2,9 +2,14 @@
 
 function executeQuery($dao) {
     switch ($_POST['query']) {
+
+        /* Select */
         case 'select' :
             echo json_encode($dao->select());
             break;
+
+
+        /* Select (paginated) */
         case 'selectPaginated' :
             if (!isset($_POST["index"]) || !isset($_POST["quantity"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -12,6 +17,9 @@ function executeQuery($dao) {
                 echo json_encode($dao->selectPaginated($_POST["index"], $_POST["quantity"]));
             }
             break;
+
+
+        /* Select (joined) */
         case 'selectJoin' :
             $condition = null;
             if (isset($_POST["condition"])) {
@@ -23,6 +31,9 @@ function executeQuery($dao) {
                 echo json_encode($dao->selectJoin($_POST['otherTable'], $_POST['field'], $_POST['otherField'], $condition));
             }
             break;
+
+
+        /* Find */
         case 'find' :
             if (!isset($_POST["id"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -30,6 +41,9 @@ function executeQuery($dao) {
                 echo json_encode($dao->find($_POST['id']));
             }
             break;
+
+
+        /* Find by condition */
         case 'findByCondition' :
             if (!isset($_POST["condition"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -37,6 +51,9 @@ function executeQuery($dao) {
                 echo json_encode($dao->findByCondition($_POST['condition']));
             }
             break;
+
+
+        /* Insert */
         case 'insert' :
             if (!isset($_POST["values"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -44,6 +61,9 @@ function executeQuery($dao) {
                 echo $dao->insert($_POST['values']);
             }
             break;
+
+
+        /* Update */
         case 'update' :
             if (!isset($_POST["id"]) || !isset($_POST["values"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -51,6 +71,9 @@ function executeQuery($dao) {
                 echo $dao->update($_POST['id'], $_POST['values']);
             }
             break;
+
+
+        /* Delete */
         case 'delete' :
             if (!isset($_POST["id"])) {
                 header("HTTP/1.0 400 Request is missing parameters");
@@ -58,8 +81,10 @@ function executeQuery($dao) {
                 echo $dao->delete($_POST['id']);
             }
             break;
+
+
         default :
-            echo 'Invalid AJAX query';
+            header("HTTP/1.0 400 Request query is invalid");
             break;
     }
 }
