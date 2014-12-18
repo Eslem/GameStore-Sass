acabf55b0527d46729c77bd5d86cfe79ade7770f
alumno <alumno@2A5PC17.fpmislata.local>
2014-12-18 11:33:19 +0100

    Merge david into develop
    Conflicts:
    	nbproject/project.properties
    	public_html/cliente/css/index.css
    	public_html/cliente/css/index.css.map
    	public_html/cliente/css/navbar.css
    	public_html/cliente/css/navbar.css.map
    	public_html/cliente/css/responsive.css
    	public_html/cliente/css/responsive.css.map
    	public_html/cliente/scss/index.scss

--- public_html/cliente/js/categories.js
+++ public_html/cliente/js/categories.js
@@ -43,14 +43,17 @@ function loadGames(categoryID) {
             $(".back-panel").removeClass("active");
             result = JSON.parse(result);
             for (var i in result) {
+                $('#divGames').hide();
                 $('#divGames').append('<div class="game" data-id="' + result[i].id
                         + '"><div class="imgBack"><img src="images/games/' + result[i].id
                         + '.jpg" alt></div><div class="diagnalA">Detalle</div><div class="info"><div>'
                         + result[i].nombre + '</div></div></div>');
+                $('#divGames').fadeIn("slow");
 
             }
             $('.game').click(function(ev) {
                 findProduct($(ev.currentTarget).attr('data-id'), function(result) {
+                    $('#divDetail').hide();
                     $('#divDetail').html('<div class="detail"><img class="imgDet" src="images/games/'
                             + result.id + '.jpg" alt><div><h3>' + result.nombre
                             + '</h3><hr/><p>' + result.descripcion + '</p>'
@@ -58,10 +61,12 @@ function loadGames(categoryID) {
                             + result.precio + '€</div><button class="diagnalA btnCB btnCB-5 btnCB-5b">'
                             + '<span>Add to cart</span></button>'
                             + '</div>');
+                    $('#divDetail').fadeIn("slow");
                 });
             });
         } else {
-            if (!$(".back-panel").hasClass("active")) $(".back-panel").addClass("active");
+            if (!$(".back-panel").hasClass("active"))
+                $(".back-panel").addClass("active");
         }
     });
 }
@@ -70,8 +75,10 @@ function getSelectedCategory() {
     var url = window.location.toString();
     if (url.indexOf('#') !== -1) {
         var categoryID = parseInt(url.slice(url.indexOf('#') + 1));
-        if (!isNaN(categoryID)) loadGames(categoryID);
-        else window.history.pushState({}, "", url.slice(0, url.indexOf('#')));
+        if (!isNaN(categoryID))
+            loadGames(categoryID);
+        else
+            window.history.pushState({}, "", url.slice(0, url.indexOf('#')));
     } else {
         $(".back-panel").addClass("active");
     }
@@ -85,11 +92,13 @@ $('document').ready(function() {
         for (var i = 0; i < categories.length; i++) {
             var category = categories[i];
 
-            if (i % 3 === 0) strHTML += '<li>';
+            if (i % 3 === 0)
+                strHTML += '<li>';
             strHTML += '<span class="gameIcon" data-id="' + category.id + '">' +
                     '<img class="genreIcon" src="images/genres/'
                     + category.nombre + '.svg"><br/>' + category.nombre + '</span>';
-            if (!i % 3 === 2) strHTML += '</li>';
+            if (!i % 3 === 2)
+                strHTML += '</li>';
         }
 
         $('.panel-categorias').html(strHTML);
