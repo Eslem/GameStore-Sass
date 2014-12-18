6863f02446c9528fa1abee24f323a833b4bf01b4
alumno <manuguionbajo@gmail.com>
2014-12-18 11:55:02 +0100

    openshift?

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,30 @@
-<?php header ("location: cliente/index.html");
+<?php
+
+
+$username = "adminyGQgBlT";
+$password = "uAZR7FyX8zh9";
+$dbname = "metroidgames";
+
+$openshiftHost = $_ENV["OPENSHIFT_MYSQL_DB_HOST"];
+$openshiftPort = $_ENV["OPENSHIFT_MYSQL_DB_PORT"];
+
+
+$servername = "mysql://$openshiftHost:$openshiftPort";
+
+
+$conn = new mysqli($openshiftHost, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//$sql = "SELECT 7  from DUAL";
+//$result = $conn->query($sql);
+
+echo json_encode($result->fetch_assoc());
+$conn->close();
+
+
+
+
+header ("location: cliente/index.html");
