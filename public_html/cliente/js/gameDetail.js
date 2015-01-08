2dff51908389fa354c963933fb294ac04cf65b79
jesuscc1993 <jesuscc1993@gmail.com>
2015-01-08 12:03:49 +0100

    La vista detalle juego ahora se adapta al contenido

--- public_html/cliente/js/gameDetail.js
+++ public_html/cliente/js/gameDetail.js
@@ -9,8 +9,9 @@ function loadGameDetail(container, gameInfo, inCart) {
     container.hide();
     var strHTML = '<div class="detail">'
             + '<img class="imgDet" src="images/games/' + gameInfo.id + '.jpg" alt>'
-            + '<div><h3>' + gameInfo.nombre + '</h3><hr/>'
-            + '<p>' + gameInfo.descripcion + '</p></div>' + '<div class="platforms">Plataformas</div>'
+            + '<div class="description"><h3>' + gameInfo.nombre + '</h3><hr/>'
+            + '<p>' + gameInfo.descripcion + '</p></div>'
+            + '<div class="bottom"><div class="platforms">Plataformas</div>'
             + '<div class="precio">' + gameInfo.precio + '€</div>';
 
     if (!inCart) strHTML += '<button onclick="addToCart(' + gameInfo.id + ');" class="diagnalA btnCB btnCB-5 btnCB-5b">'
@@ -18,6 +19,8 @@ function loadGameDetail(container, gameInfo, inCart) {
 
     else strHTML += '<button onclick="removeFromCart(' + gameInfo.id + ', loadCart);" class="diagnalA btnCB btnCB-5 btnCB-5b">'
                 + '<span>Remove from cart</span></button></div>';
+
+    strHTML += '</div>';
 
     container.html(strHTML);
     container.fadeIn("slow");
