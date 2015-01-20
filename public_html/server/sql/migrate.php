7ffa4842eda222494306a7d3574aa952d3a7cc97
hermanu <manuguionbajo@gmail.com>
2015-01-20 20:57:22 +0100

    algo mas

--- public_html/server/sql/migrate.php
+++ public_html/server/sql/migrate.php
@@ -2,11 +2,19 @@
 
 require "flyway.php";
 
-$openshiftHost = $_ENV["OPENSHIFT_MYSQL_DB_HOST"];
-$openshiftPort = $_ENV["OPENSHIFT_MYSQL_DB_PORT"];
-echo 'mysql://' . $openshiftHost . ':' . $openshiftPort;
-$link = mysqli_connect($openshiftHost . ':' . $openshiftPort, 'adminXFr3dCn', 'vGhykHT4Ph2v', 'metro')
+//$openshiftHost = $_ENV["OPENSHIFT_MYSQL_DB_HOST"];
+//$openshiftPort = $_ENV["OPENSHIFT_MYSQL_DB_PORT"];
+//echo 'mysql://' . $openshiftHost . ':' . $openshiftPort;
+//$link = mysqli_connect($openshiftHost . ':' . $openshiftPort, 'adminXFr3dCn', 'vGhykHT4Ph2v', 'metro')
+ //       or die("Error link" . mysqli_error($link));
+
+$link = mysqli_connect('localhost', 'root', '', 'tienda')
         or die("Error link" . mysqli_error($link));
+
+
+
+
+
 
 $flyway = new Flyway($link);
 $flyway->migrate();
