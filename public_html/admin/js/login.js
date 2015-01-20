fe449f122716e8e0d090e1f5277d496c9cc937e7
eslem <j.eslem03@gmail.com>
2015-01-20 17:56:25 +0100

    loading animation

---
+++ public_html/admin/js/login.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function login(elem){
+   // alert();
+    $("#logo").addClass("flip");
+    setInterval(function(){
+        
+    });
+    $(elem).text("Loading...");
+}
