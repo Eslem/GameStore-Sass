3b527a7cb717e187ce0877cccab91ee8b0e77d53
Manu <Manu@laptop>
2015-01-27 00:21:19 +0100

    registro popup 0%

--- public_html/cliente/registro/registro.js
+++ public_html/cliente/registro/registro.js
@@ -1,21 +1,18 @@
 
 function loadRegistro() {
 
-    $("#registroClientes").load("registro/registro.html").dialog({modal:true});
-
-/*$("#registroClientes").dialog({
-      autoOpen: false,
-      height: 300,
-      width: 350,
-      modal: true,
-      buttons: {
-         Cancel: function() {
-          $("#registroClientes").dialog( "close" );
+    $("#registroClientes").load("registro/registro.html").dialog({
+        autoOpen: true,
+        modal: true,
+        buttons: {
+            Cancel: function () {
+                $("#registroClientes").dialog("close");
+            }
+        },
+        close: function () {
+            $("#registroClientes input").text("");
         }
-      },
-      close: function() {
-       $("#registroClientes input").value("");
-      }
-    });*/
+    });
+
 };
 
