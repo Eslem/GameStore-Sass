9c6530b8e4d41d6b483e39c1745e766a01f46b96
jesuscc1993 <jesuscc1993@gmail.com>
2015-02-18 12:07:03 +0100

    Test de JQGrid funcionando.

--- public_html/tests/jqgrid/jqgrid.php
+++ public_html/tests/jqgrid/jqgrid.php
@@ -1,16 +1,17 @@
 <?php
-//ini_set('display_errors', 0);
 $page = $_GET['page']; // get the requested page
 $limit = $_GET['rows']; // get how many rows we want to have into the grid
 $sidx = $_GET['sidx']; // get index row - i.e. user click to sort
 $sord = $_GET['sord']; // get the direction
+
+$tableName = 'categoria';
 
 if (!$sidx) {
     $sidx = 1;
 }
 $db = mysqli_connect('localhost', 'root', 'root', 'tienda') or die("Connection Error: " . mysql_error());
 
-$result = mysqli_query($db, "SELECT COUNT(*) AS count FROM producto");
+$result = mysqli_query($db, "SELECT COUNT(*) AS count FROM $tableName");
 while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
     $count = $row['count'];
 }
@@ -25,8 +26,8 @@ if ($page > $total_pages) {
 }
 
 $start = $limit * $page - $limit; // do not put $limit*($page - 1)
-$SQL = "SELECT * FROM producto ORDER BY $sidx $sord LIMIT $start , $limit";
-$result = mysqli_query($db, $SQL) or die("Couldn t execute query." . mysql_error());
+$SQL = "SELECT * FROM $tableName ORDER BY $sidx $sord LIMIT $start , $limit";
+$result = mysqli_query($db, $SQL) or die("Couldn't execute query." . mysql_error());
 
 
 $response = new stdClass;
@@ -37,6 +38,9 @@ $i = 0;
 while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
     //$response->rows[$i]['id'] = $row[id];
     $response->rows[$i]['cell'] = $row;
+    //echo print_r($row);
     $i++;
 }
+
+//print_r($response);
 echo json_encode($response);
